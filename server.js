deaba060ae1c2051b5c7755180f9f013740b7568
Nemanja Vukosavljevic <nemanja06@gmail.com>
2015-01-28 20:26:43 +0100

    finally managed to get the express session in socket

--- server.js
+++ server.js
@@ -15,19 +15,16 @@ var sessionMiddleware = session({
   store: sessionStore, // MemoryStore
   secret: 's3cr37',
   saveUninitialized: true,
-  resave: true,
+  resave: false,
 });
 
 
+//a way to pass the same session in both express and socket.io
 io.use(function(socket, next) {
-  console.log('going through socketio middleware');
-  // console.log(socket.request);
-  // console.log(socket.request.res);
   sessionMiddleware(socket.request, socket.request.res, next);
 });
 
 app.use(sessionMiddleware);
-// app.use(cookieParser);
 // parse application/json
 app.use(bodyParser.json())
 
@@ -38,31 +35,30 @@ server.listen(port, function() {
 // Routing
 app.use(express.static(__dirname + '/public'));
 
-// Chatroom
-
 // usernames which are currently connected to the chat
 var usernames = {};
 var numUsers = 0;
 
+var getUsernameFromSession = function(socketRequest) {
+  var sessionId = socketRequest.sessionID;
+  var cookie = JSON.parse(socketRequest.sessionStore.sessions[sessionId]);
+  return cookie.username;
+};
+
 app.post('/login', function(req, res){
   req.session.username = req.body.user;
-  console.log("REQ.SESSION.USERNAME");
-  console.log(req.session);
   res.sendStatus(200);
 });
 
 io.on('connection', function(socket) {
   console.log('SESSION');
-  console.log(socket.request.session);
-  console.log(socket.request.session.username);
-  console.log('connection ' + socket.username + ' is in onconnection');
   var addedUser = false;
 
   // when the client emits 'new message', this listens and executes
   socket.on('new message', function(data) {
-    console.log('on new msg ' + socket.request.session.username);
+    var usernameSession = getUsernameFromSession(socket.request);
     var date = new Date();
-    console.log('from ' + socket.request.session.username + ': ' + data.text + ' at ' + data.datetime);
+    console.log('from ' + usernameSession + ': ' + data.text + ' at ' + data.datetime);
     console.log('server time is ' + date.getTime() + ' and offset ' + date.getTimezoneOffset() + ' and client time is ' + data.datetime + ' with offset ' + data.offset);
     console.log('number of users is ' + Object.keys(usernames).length);
 
@@ -70,7 +66,7 @@ io.on('connection', function(socket) {
       if(!err) {
         // console.log("We are connected");
         var collection = db.collection('chat');
-        collection.insert({ username: socket.request.session.username, message: data }, function (err, o) {
+        collection.insert({ username: usernameSession, message: data }, function (err, o) {
           if (err) {
             console.warn(err.message);
           } else {
@@ -82,19 +78,14 @@ io.on('connection', function(socket) {
 
     // we tell the client to execute 'new message'
     socket.broadcast.emit('new message', {
-      username: socket.request.session.username,
+      username: usernameSession,
       message: data
     });
   });
 
   // when the client emits 'add user', this listens and executes
   socket.on('add user', function(username) {
-    console.log(username + ' is connected with add user');
-    console.log(socket.request.session);
-    // we store the username in the socket session for this client
-    // socket.username = username;
-    // socket.request.session.username = username;
-    console.log('ADD USER val from session ' + socket.request.session.username);
+    var usernameSession = getUsernameFromSession(socket.request);
     // add the client's username to the global list
     usernames[username] = username;
     ++numUsers;
@@ -104,32 +95,40 @@ io.on('connection', function(socket) {
     });
     // echo globally (all clients) that a person has connected
     socket.broadcast.emit('user joined', {
-      username: socket.request.session.username,
+      username: usernameSession,
       numUsers: numUsers
     });
 
     mongo.connect("mongodb://localhost:27017/mydb", function (err, db) {
       var collection = db.collection('chat')
-      var stream = collection.find().sort({ "message.datetime" : -1 }).limit(10).stream();
-      stream.on('data', function (elem) {
-        // console.log(elem);
-        // console.log('broadcasting new message ' + elem.username + ' ' + elem.message.text + ' ' + elem.message.datetime + ' ' + elem.message.offset);
-        socket.emit('new message', elem);
+      // var stream = collection.find().sort({ "message.datetime" : -1 }).limit(10).stream();
+      // stream.on('data', function (elem) {
+        // socket.emit('new message', elem);
+      // });
+      collection.find().sort({ "message.datetime" : -1}).limit(10).toArray(function(err, messages){
+        if (err){
+          return;
+        }
+        for (var i = messages.length-1; i >= 0; --i) {
+          socket.emit('new message', messages[i]);
+        }
       });
     });
   });
 
   // when the client emits 'typing', we broadcast it to others
   socket.on('typing', function() {
+    var usernameSession = getUsernameFromSession(socket.request);
     socket.broadcast.emit('typing', {
-      username: socket.request.session.username
+      username: usernameSession
     });
   });
 
   // when the client emits 'stop typing', we broadcast it to others
   socket.on('stop typing', function() {
+    var usernameSession = getUsernameFromSession(socket.request);
     socket.broadcast.emit('stop typing', {
-      username: socket.request.session.username
+      username: usernameSession
     });
   });
 
